72803cd09892c335f80a325b22cb5f1fcbd1c553
tmoore28 <mooretylor28@gmail.com>
2021-10-13 21:49:36 -0400

    changed hello world message to hey glad to be in the c-sharp class

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HEY! happy to join the c-sharp class");
+        }
+    }
+}
